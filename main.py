a0d8775560ceedf3bcd9157f3a35a11a8debfa95
Борис Сухобрус <bhobrus@gmail.com>
2022-04-11 19:25:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import pygame
+import random
+
+class Snake:
+    def __init__(self, x, y, color, speed, size):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.speed = speed
+        self.size = size
+
+    def move_right(self):
+        self.x += self.speed
+
+    def move_left(self):
+        self.x -= self.speed
+
+    def move_down(self):
+        self.y += speed
+
+    def move_up(self):
+        self.y -= speed
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
+
+    def check_walls(self):
+        if self.x <= 0:
+            self.move_right()
+        if self.y <= 0:
+            self.move_down()
+        if self.y >= HEIGHT:
+            self.move_up()
+        if self.x >= WIDTH:
+            self.move_left()
+
+    def check_food(self, food_x, food_y):
+        if self.x == food_x and self.y == food_y:
+            return True
+        return False
+
+WIDTH = 720
+HEIGHT = 480
+
+Black = (0,0,0)
+White = (255,255,255)
+Red = (255,0,0)
+Lime = (0,255,0)
+Blue= (0,0,255)
+
+sr = pygame.display.set_mode((WIDTH,HEIGHT))
+
+pygame.display.set_caption('SkySmart Game')
+image = pygame.image.load('skyicon.PNG')
+pygame.display.set_icon(image)
+
+fps = 20 # frames per second
+clock = pygame.time.Clock()
+
+is_key_right = False
+is_key_left = False
+is_key_down = False
+is_key_top = False
+
+speed = 15 # Скорость должна быть равной размеру
+size = 15
+
+width_hero = 15
+height_hero = 15
+
+food_x = 150
+food_y = 150
+
+is_eat = True
+
+snake = Snake(3*speed, 3*speed, Red, speed, size)
+while True:
+    sr.fill(Black)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                is_key_right = True
+            if event.key == pygame.K_LEFT:
+                is_key_left = True
+            if event.key == pygame.K_UP:
+                is_key_top = True
+            if event.key == pygame.K_DOWN:
+                is_key_down = True
+
+    if is_key_right:
+        snake.move_right()
+    if is_key_left:
+        snake.move_left()
+    if is_key_top:
+        snake.move_up()
+    if is_key_down:
+        snake.move_down()
+
+    snake.check_walls()
+    is_eat = snake.check_food(food_x, food_y)
+    snake.draw(sr)
+    if is_eat:
+        food_x = random.randint(0, WIDTH) * speed % WIDTH
+        food_y = random.randint(0, HEIGHT) * speed % HEIGHT
+
+    pygame.draw.rect(sr, Blue, (food_x, food_y, size, size))
+    pygame.display.update()
+    clock.tick(fps)
